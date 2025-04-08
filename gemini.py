79461104cd4cfcf8ad33a2a9c6766a47d79ed379
Abdulaziz <uzuzmovie@gmail.com>
2025-04-08 19:32:10 +0500

    Initial commit

--- gemini.py
+++ gemini.py
@@ -3,20 +3,15 @@ import os
 import telebot
 import google.generativeai as genai
 
-# .env faylini yuklaymiz
 load_dotenv()
 
-# API kalitlarni os.environ orqali yuklaymiz
 bot_api_key = os.getenv("TELEGRAM_API_KEY")
 gemini_api_key = os.getenv("GEMINAI_API_KEY")
 
-# Telegram botini API kaliti bilan ishga tushiramiz
 bot = telebot.TeleBot(bot_api_key, parse_mode=None)
 
-# Gemini AI uchun konfiguratsiya qilamiz
 genai.configure(api_key=gemini_api_key)
 
-# GeminAI uchun boshqa kodlar
 generation_config = {
     "temperature": 1,
     "top_p": 0.95,
